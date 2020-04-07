e4d87bbee48da1377125b3bb751bee7ebdf61341
Benjamin Abbitt <benjamin.abbitt@gmail.com>
2020-04-07 15:54:39 -0500

    initial working unit test for command handler Handle, including transports

--- applications/commandHandler/framework/server_test.go
+++ applications/commandHandler/framework/server_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/benjaminabbitt/evented/support"
 	"github.com/benjaminabbitt/evented/transport"
 	"github.com/benjaminabbitt/evented/transport/async"
+	"github.com/benjaminabbitt/evented/transport/async/mock"
 	"github.com/benjaminabbitt/evented/transport/sync/projector"
 	"github.com/benjaminabbitt/evented/transport/sync/saga"
 	"github.com/golang/protobuf/ptypes"
@@ -58,6 +59,103 @@ func (s ServerSuite) Test_Handle() {
 	holder.On("GetSaga").Return([]saga.SyncSaga{})
 	holder.On("GetTransports").Return([]async.Transport{})
 	server.Handle(context.Background(), commandBook)
+	holder.AssertExpectations(s.T())
+	businessClient.AssertExpectations(s.T())
+	eventBookRepo.AssertExpectations(s.T())
+}
+
+func (s ServerSuite) Test_HandleWithTransports() {
+	eventBookRepo := new(eventBook.MockEventBookRepository)
+	holder := new(transport.MockHolder)
+	businessClient := new(client.MockClient)
+	server := NewServer(eventBookRepo, holder, businessClient, s.log, s.errh)
+
+	commandBook := s.produceCommandBook()
+
+	id, _ := evented_proto.ProtoToUUID(*commandBook.Cover.Root)
+	eventBookRepo.On("Get", id).Return(*s.produceHistoricalEventBook(*commandBook), nil)
+
+	contextualCommand := &eventedcore.ContextualCommand{
+		Events:  s.produceHistoricalEventBook(*commandBook),
+		Command: commandBook,
+	}
+
+	businessResponse := s.produceBusinessResponse(*commandBook)
+	businessClient.On("Handle", contextualCommand).Return(businessResponse, nil)
+	eventBookRepo.On("Put", *businessResponse).Return(nil)
+
+	var syncEventPages []*eventedcore.EventPage
+	syncEventPages = append(syncEventPages, &eventedcore.EventPage{
+		Sequence:    &eventedcore.EventPage_Num{Num: 0},
+		CreatedAt:   nil,
+		Event:       nil,
+		Synchronous: false,
+	}, &eventedcore.EventPage{
+		Sequence:    &eventedcore.EventPage_Num{Num: 1},
+		CreatedAt:   nil,
+		Event:       nil,
+		Synchronous: true,
+	})
+
+	syncEventBook := &eventedcore.EventBook{
+		Cover:    businessResponse.Cover,
+		Pages:    syncEventPages,
+		Snapshot: nil,
+	}
+
+	projection := &eventedcore.Projection{
+		Cover:      syncEventBook.Cover,
+		Projector:  "test",
+		Sequence:   0,
+		Projection: nil,
+	}
+
+	mockProjector := new(projector.MockProjectorClient)
+	mockProjector.On("HandleSync", syncEventBook).Return(projection, nil)
+	holder.On("GetProjections").Return([]projector.SyncProjection{mockProjector})
+
+	sagaResult := &eventedcore.EventBook{
+		Cover:    nil,
+		Pages:    nil,
+		Snapshot: nil,
+	}
+
+	mockSaga := new(saga.MockSagaClient)
+	mockSaga.On("HandleSync", syncEventBook).Return(sagaResult, nil)
+	holder.On("GetSaga").Return([]saga.SyncSaga{mockSaga})
+
+	var asyncEventPages []*eventedcore.EventPage
+	asyncEventPages = append(asyncEventPages, &eventedcore.EventPage{
+		Sequence:    &eventedcore.EventPage_Num{Num: 0},
+		CreatedAt:   nil,
+		Event:       nil,
+		Synchronous: false,
+	}, &eventedcore.EventPage{
+		Sequence:    &eventedcore.EventPage_Num{Num: 1},
+		CreatedAt:   nil,
+		Event:       nil,
+		Synchronous: true,
+	}, &eventedcore.EventPage{
+		Sequence:    &eventedcore.EventPage_Num{Num: 2},
+		CreatedAt:   nil,
+		Event:       nil,
+		Synchronous: false,
+	})
+
+	asyncEventBook := &eventedcore.EventBook{
+		Cover:    businessResponse.Cover,
+		Pages:    asyncEventPages,
+		Snapshot: nil,
+	}
+
+	mockTransport := new(mock.AsyncTransport)
+	mockTransport.On("Handle", asyncEventBook).Return(nil)
+	holder.On("GetTransports").Return([]async.Transport{mockTransport})
+
+	server.Handle(context.Background(), commandBook)
+	holder.AssertExpectations(s.T())
+	businessClient.AssertExpectations(s.T())
+	eventBookRepo.AssertExpectations(s.T())
 }
 
 func (s ServerSuite) produceBusinessResponse(commandBook eventedcore.CommandBook) *eventedcore.EventBook {
@@ -70,6 +168,11 @@ func (s ServerSuite) produceBusinessResponse(commandBook eventedcore.CommandBook
 		Synchronous: false,
 	}, &eventedcore.EventPage{
 		Sequence:    &eventedcore.EventPage_Num{Num: 1},
+		CreatedAt:   nil,
+		Event:       nil,
+		Synchronous: true,
+	}, &eventedcore.EventPage{
+		Sequence:    &eventedcore.EventPage_Num{Num: 2},
 		CreatedAt:   nil,
 		Event:       nil,
 		Synchronous: false,
@@ -121,9 +224,6 @@ func (s ServerSuite) produceCommandBook() *eventedcore.CommandBook {
 	return commandBook
 }
 
-
-
 func TestServerSuite(t *testing.T) {
 	suite.Run(t, new(ServerSuite))
 }
-
